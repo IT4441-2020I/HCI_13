3f6cc7b9cebf413921be81a2d374ff803200306c
Van Vu Nguyen <nvvu6999@gmail.com>
2020-12-16 19:56:42 +0700

    Update Tiendo page

--- src/components/BaiHoc/LessionPracticeContent.js
+++ src/components/BaiHoc/LessionPracticeContent.js
@@ -3,15 +3,21 @@ import Hands from "../keyboard/Hands";
 import StenoInput from "../keyboard/StenoInput";
 import QwertyStenoKeyboad from "../keyboard/qwerty-steno-keyboard/QwertyStenoKeyboard";
 import { MdArrowForward, MdArrowBack } from "react-icons/md";
+import { useRef } from "react";
 
 function LessionPracticeContent(props) {
     const { hidePracticeContent } = props;
+    const requireKeyRef = useRef();
     const pressedKeys = new Set();
 
     const handleKeyDown = (event) => {
         const { key } = event;
         pressedKeys.add(key);
-        event.target.value = [...pressedKeys].join("");
+        const pressedWord = [...pressedKeys].join("");
+        event.target.value = pressedWord;
+        if (pressedWord.toUpperCase() === requireKeyRef.current.innerText) {
+            generateKey();
+        }
     };
 
     const handleKeyUp = (event) => {
@@ -20,14 +26,28 @@ function LessionPracticeContent(props) {
         event.target.value = [...pressedKeys].join("");
     };
 
+    const handleChange = (event) => {
+        event.target.value = [...pressedKeys].join("");
+    };
+
+    const generateKey = () => {
+        if (requireKeyRef.current.innerText === "S") {
+            requireKeyRef.current.innerText = "K";
+        } else {
+            requireKeyRef.current.innerText = "S";
+        }
+    };
+
     return (
         <div className="lession-practice-wrapper">
             <button id="hide-practice-button" onClick={hidePracticeContent}>
                 <MdArrowBack size="1.5em" />
                 Quay lại bài học
             </button>
-            <div className="require-key">S</div>
-            <button id="next-key">
+            <div ref={requireKeyRef} className="require-key">
+                S
+            </div>
+            <button id="next-key" onClick={generateKey}>
                 Phím khác
                 <MdArrowForward size="20px" />
             </button>
@@ -35,6 +55,7 @@ function LessionPracticeContent(props) {
                 parentSelector=".lession-practice-wrapper"
                 onKeyDownHook={handleKeyDown}
                 onKeyUpHook={handleKeyUp}
+                onChangeHook={handleChange}
             />
             <div className="keyboards">
                 <QwertyStenoKeyboad style={{ fontSize: "13px" }} />
