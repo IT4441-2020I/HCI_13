223c8db6341d3a8a128089b1aebf96048dbdd714
Van Vu Nguyen <nvvu6999@gmail.com>
2020-12-17 10:04:31 +0700

    Add success toast after click the copy icon

--- src/components/soanthao/Soanthao.js
+++ src/components/soanthao/Soanthao.js
@@ -2,9 +2,13 @@ import "./soanthao.css";
 import StenoInput from "../keyboard/StenoInput";
 import QwertyStenoKeyboard from "../keyboard/qwerty-steno-keyboard/QwertyStenoKeyboard";
 import StenoKeyboard from "../keyboard/steno-keyboard/StenoKeyboard";
-import { MdContentCopy } from "react-icons/md";
+import { MdContentCopy, MdCheckCircle } from "react-icons/md";
+import { Toast, ToastBody } from "react-bootstrap";
+import { useState } from "react";
 
 function Soanthao() {
+    const [toastShow, setToastShow] = useState(false);
+
     const copyToClipboard = () => {
         const inputField = document.querySelector(
             ".soanthao .input-area textarea"
@@ -13,17 +17,47 @@ function Soanthao() {
             inputField.select();
             document.execCommand("copy");
         }
+        setToastShow(true);
     };
 
     return (
         <div className="soanthao">
             <div className="input-area">
                 <StenoInput type={"textarea"} />
-                <MdContentCopy size="2.5em" onClick={copyToClipboard} />
-            </div>
-            <div className="keyboards">
-                <QwertyStenoKeyboard style={{ fontSize: 14 }} />
-                <StenoKeyboard style={{ fontSize: 18 }} />
+                <MdContentCopy
+                    id="copy-icon"
+                    size="2.5em"
+                    onClick={copyToClipboard}
+                />
+                <Toast
+                    onClose={() => setToastShow(false)}
+                    show={toastShow}
+                    delay={1500}
+                    autohide
+                    style={{
+                        position: "absolute",
+                        right: 0,
+                        top: "3em",
+                        borderLeft: "5px solid #28A745",
+                    }}
+                >
+                    <Toast.Body>
+                        <MdCheckCircle
+                            color="#28A745"
+                            size="1.5em"
+                            style={{
+                                float: "left",
+                                position: "relative",
+                                margin: "auto",
+                            }}
+                        />
+                        Sao chép thành công!
+                    </Toast.Body>
+                </Toast>
+                <div className="keyboards">
+                    <QwertyStenoKeyboard style={{ fontSize: 14 }} />
+                    <StenoKeyboard style={{ fontSize: 18 }} />
+                </div>
             </div>
         </div>
     );
